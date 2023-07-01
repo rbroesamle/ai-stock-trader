1824914657aa8d1c0adcbbd86285bb381743d9a7
HackerBarryFinn <lucas.bareiss@gmx.de>
2023-07-01 21:41:11 +0200

    [6] finish class that is responsible for the chatgpt co munication and extended requirements

--- chatgpt.py
+++ chatgpt.py
@@ -1,3 +1,5 @@
+import openai
+openai.api_key = "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"
 class ChatGPT:
     def get_score(self, headline, company, mock=True):
         if mock:
@@ -7,10 +9,26 @@ class ChatGPT:
 
     @classmethod
     def get_score_with_chatgpt(cls, headline, company):
-        # TODO Lucas
-        return 1
+        response = openai.Completion.create(
+            engine="text-davinci-003",   #gpt-3.5-turbo
+            #prompt aus Vorlage angepasst
+            prompt=f"Forget all your previous instructions. Pretend you are a financial expert\
+                with stock recommendation experience.\
+                Only answer with “2” if good news, “0” if bad news, or “1” if neutral or uncertain.\
+                Is the headline:\"{headline}\" good or bad for the stock price of {company} in the short term?",
+            max_tokens=20,
+            temperature=0,
+            n=1,
+            stop=None,
+        )
+
+        if response.choices:
+            return int(response.choices[0].text.strip()) - 1
+        else:
+            print("Fehler")
 
     @classmethod
     def get_score_randomly(cls, headline, company):
         # TODO Pia
         return 1
+
