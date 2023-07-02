49f3201ddd2da8773e10d3bf1b3d82ff0971b105
renzixilef <felix.renzi@gmx.de>
2023-07-02 02:24:00 +0200

    implement function that returnes percentage increase of actual stock and begin implementation of lin regression

---
+++ lin_regression.py
@@ -0,0 +1,10 @@
+import numpy as np
+from sklearn.linear_model import LinearRegression
+
+
+def get_x_y_array_from_companies(all_companies):
+    # all_x_y_tuples = []
+    for company in all_companies:
+        for score in company.average_score:
+            break
+        # all_tuples.append
